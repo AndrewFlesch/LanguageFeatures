b4c63608711ead25956c4ab26bcc2ff38946a1ad
AndrewFlesch <aflesch@outlook.com>
2018-03-13 09:50:45 -0500

    Lambda Expressions

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -10,27 +10,56 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
+       
+        //public ViewResult Index()
+        //{
+        //    ShoppingCart cart = new ShoppingCart { Products = Product.GetProduct() };
+        //    decimal cartTotal = cart.TotalPrices();
+        //    return View("Index", new string[] { $"Total: {cartTotal:c2}" });
+        //}
+
+        //public ViewResult Index()
+        //{
+        //    ShoppingCart cart = new ShoppingCart
+        //    {
+        //        Products = Product.GetProduct()
+        //    };
+        //    Product[] productArray =
+        //    {
+        //        new Product
+        //        {
+        //            Name = "Kayak", Price = 275M },
+        //        new Product
+        //        {
+        //            Name = "LifeJacket", Price = 48.95M }
+        //    };
+        //    decimal cartTotal = cart.TotalPrices();
+        //    decimal arrayTotal = productArray.TotalPrices();
+        //    return View("Index", new string[] {
+        //        $"Cart Total: {cartTotal:c2}",$"Array total: {arrayTotal:c2}" 
+        //    });
+        //} 
+
         public ViewResult Index()
         {
-            object[] data = new object[]
+            Product[] productArray =
             {
-                275M, 29.95M, "apple","orange", 100, 10
+                new Product {Name = "kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M},
             };
-            decimal total = 0;
-            for (int i = 0; i < data.Length; i++)
-            {
-              switch (data[i])
-                {
-                    case decimal decimalValue:
-                        total += decimalValue;
-                        break;
-                    case int intValue when intValue > 50:
-                        total += intValue;
-                        break;
 
-                }
-            }
-            return View("Index", new string[] { $"Total: {total:c2}" });
+
+            decimal priceFilterTotal = productArray
+                .Filter(p => (p?.Price ?? 0) >= 20)
+                .TotalPrices();
+
+            decimal nameFiltertotal = productArray
+               .Filter(p => p?.Name?[0] == 'S')
+               .TotalPrices();
+
+            return View("Index", new string[] { $"Array Total: {arraytotal:c2}", $"Name Total: {nametotal:c2}" });
         }
     }
 }
